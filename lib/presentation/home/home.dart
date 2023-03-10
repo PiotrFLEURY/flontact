4df7fe25b9ed7f25c8d47174d9e059c1ed8e693b
PiotrFLEURY <piotr.fleury@gmail.com>
2023-03-10 14:18:04 +0100

    load data on screen display

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -3,26 +3,33 @@ import 'package:flontacts/presentation/states/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ContactsPage extends ConsumerWidget {
+class ContactsPage extends ConsumerStatefulWidget {
   const ContactsPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ContactsPage> createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends ConsumerState<ContactsPage> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(contactListProvider.notifier).refresh();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final contacts = ref.watch(contactListProvider);
 
     return Scaffold(
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          return ContactView(contact: contacts[index]);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(contactListProvider.notifier).refresh();
-        },
-        tooltip: 'Refresh',
-        child: const Icon(Icons.refresh),
+      body: RefreshIndicator(
+        onRefresh: () => ref.read(contactListProvider.notifier).refresh(),
+        child: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return ContactView(contact: contacts[index]);
+          },
+        ),
       ),
     );
   }
